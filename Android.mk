eecaaeb0136f8fcdd96233180820789d3307ac34
Luke Benstead <kazade@gmail.com>
2014-09-27 11:51:08 +0100

    Initial commit

---
+++ Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE	:= lua
+LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/src/*.c)
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/src
+LOCAL_SRC_FILES := $(LOCAL_SRC_FILES:$(LOCAL_PATH)/%=%)
+LOCAL_CFLAGS	:= "-std=c99"
+include $(BUILD_SHARED_LIBRARY)
